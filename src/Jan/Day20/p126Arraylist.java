da27f8da83d102e6b2e43e2ddce48e71f4891212
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-01-20 14:33:10 +0800

    第一次提交p126

--- src/Jan/Day20/p126Arraylist.java
+++ src/Jan/Day20/p126Arraylist.java
@@ -18,6 +18,7 @@ public class p126Arraylist {
         people[1]=p2;
         people[2]=p3;
         System.out.println(people);
+        System.out.println("=========服务器连接失败，Push failed");
 
         System.out.println(people[0]);
         System.out.println(people[1]);
